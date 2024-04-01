6391c70c1158cf51dfa910f933ce91ddb3facaaf
Jhosep Ricardo Varela Regalado <jregalado@uninorte.edu.co>
2024-04-01 13:48:39 -0500

    Test: Conflict

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,9 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        float asd=4;
+        int rew=3;
+        System.out.println((rew<<1)==asd);
+        System.out.println("Test:Conflict");
     }
 }
